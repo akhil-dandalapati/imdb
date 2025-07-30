9f8e6b1360a281f3e9fcd034724a390db2033c18
akhil <aforakhil212@gmail.com>
2025-07-30 17:43:32 +0530

    Initial commit

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./movieList.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Card from "../cardfolder/card";
+import Cards from "../cardfolder/card";
 
 const MovieList = () => {
   const [movieList, setMovieList] = useState([]);
@@ -36,7 +36,7 @@ const MovieList = () => {
         }}
       >
         {movieList.map((movie) => (
-          <Card movie={movie} key={movie.id} />
+          <Cards movie={movie} key={movie.id} />
         ))}
       </div>
     </div>
